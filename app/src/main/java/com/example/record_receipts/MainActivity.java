e2c4ac33561ec320fc63951fac3c92e1df5dbb4a
Tom Cook <tcook@email.wm.edu>
2022-09-29 01:17:05 -0400

    base camera functionality and upload image functionality

--- app/src/main/java/com/example/record_receipts/MainActivity.java
+++ app/src/main/java/com/example/record_receipts/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.record_receipts;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String currentPhotoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityResultLauncher<Uri> mTakePicture = registerForActivityResult(new ActivityResultContracts.TakePicture(), result -> {
+            return;
+        });
+        ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri result) {
+                        return;
+                    }
+                });
+
+        File photo = createImageFile();
+        Uri photo_uri = FileProvider.getUriForFile(getApplicationContext(), "com.example.android.fileprovider", photo);
+
+        Button take_photo = findViewById(R.id.activity_main_take_button);
+        take_photo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mTakePicture.launch(photo_uri);
+            }
+        });
+
+        Button upload_photo = findViewById(R.id.activity_main_upload_button);
+        upload_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGetContent.launch("image/*");
+            }
+        });
     }
-}
+
+    private File createImageFile(){
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+        }
